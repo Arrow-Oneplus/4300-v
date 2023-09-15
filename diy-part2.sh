dd946602344820916c46c9927658b4859c5082e3
chen99 <93034902+Dr-chen99@users.noreply.github.com>
2023-09-15 09:06:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,5 +15,11 @@
 sed -i '/TARGET_LDFLAGS += -lubox -lubus/i\TARGET_CFLAGS += -ffunction-sections -fdata-sections -flto' package/network/services/hostapd/Makefile
 sed -i '/TARGET_LDFLAGS += -lubox -lubus/i\TARGET_LDFLAGS += -Wl,--gc-sections -flto=jobserver -fuse-linker-plugin' package/network/services/hostapd/Makefile
 sed -i 's/192.168.1.1/192.168.5.31/g' package/base-files/files/bin/config_generate
-git clone -b main https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
-git clone -b main https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+#svn export --force https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox package/op-homebox
+svn export --force https://github.com/kenzok8/small-package/trunk/filebrowser package/filebrowser
+svn export --force https://github.com/kenzok8/small-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+svn export --force https://github.com/kenzok8/small-package/trunk/luci-app-tcpdump package/luci-app-tcpdump
+svn export --force https://github.com/kenzok8/small-package/trunk/luci-app-packet-capture package/luci-app-packet-capture
+svn export --force https://github.com/kenzok8/small-package/trunk/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone -b main git@github.com:Dr-chen99/luci-app-parentcontrol.git package/luci-app-parentcontrol
+git clone -b main git@github.com:Dr-chen99/luci-app-eqosplus.git package/luci-app-eqosplus
