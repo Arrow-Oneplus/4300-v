ef0774b59c850b8b2d2c21a665b812b9a6449bd0
chen99 <93034902+Dr-chen99@users.noreply.github.com>
2023-09-15 08:44:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git sbwml https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
+#echo 'src-git sbwml https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
+#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
+#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
+#echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
